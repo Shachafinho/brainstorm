f020fd0a262b44e904ef69b8631e9a4d637b2e7d
Shachaf Ben Jakov <shachafinho@gmail.com>
2020-02-18 13:16:09 +0200

    .mind reader is now "re-entrant"

--- brainstorm/sample/mind/reader.py
+++ brainstorm/sample/mind/reader.py
@@ -7,17 +7,30 @@ from .user_information import UserInformationStruct
 
 class BinaryStreamReader:
     def __init__(self, stream):
-        self.stream = stream
-        self.user_information = self._read_user_information()
+        self._stream = stream
+        self._user_information = None
+        self._user_information_end_location = None
 
     def _read_user_information(self):
-        return UserInformationStruct.parse_stream(self.stream)
+        return UserInformationStruct.parse_stream(self._stream)
+
+    def _update_user_information(self):
+        if not self._user_information:
+            self._user_information = self._read_user_information()
+            self._user_information_end_location = self._stream.tell()
+
+    @property
+    def user_information(self):
+        self._update_user_information()
+        return self._user_information
 
     def _read_snapshot(self):
-        return SnapshotStruct.parse_stream(self.stream)
+        return SnapshotStruct.parse_stream(self._stream)
 
     @property
     def snapshots(self):
+        self._update_user_information()
+        self._stream.seek(self._user_information_end_location)
         with contextlib.suppress(construct.StreamError):
             while True:
                 yield self._read_snapshot()
