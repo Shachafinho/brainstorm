909bdaeaeb9522ad12af95e6b31fb523106f78e8
Shachaf Ben Jakov <shachafinho@gmail.com>
2020-05-27 18:23:34 +0300

    Fix collection visuals to be bearable

--- brainstorm/gui/app/src/collection.js
+++ brainstorm/gui/app/src/collection.js
@@ -1,37 +1,70 @@
 import React from 'react';
 import Collapse from '@material-ui/core/Collapse';
-import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+// import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ChevronRight from '@material-ui/icons/ChevronRight';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListSubheader from '@material-ui/core/ListSubheader';
+
+import PropTypes from 'prop-types';
+import { fade, makeStyles } from '@material-ui/core/styles';
+import { useSpring, animated } from 'react-spring';
 
 // import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 // import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 // import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 
-import { useStyles } from './style.js';
-
 // === Components ===
 
-function CollectionItem(props) {
-  const [summary, ...details] = props.children;
+function TransitionComponent(props) {
+  const style = useSpring({
+    from: { opacity: 0, transform: 'translate3d(40px,0,0)' },
+    to: { opacity: props.in ? 1 : 0, transform: `translate3d(${props.in ? 0 : 40}px,0,0)` },
+  });
+
   return (
-    <>
-      <ListItem button onClick={props.onClick}>
+    <animated.div style={style}>
+      <Collapse {...props} />
+    </animated.div>
+  );
+}
+TransitionComponent.propTypes = {
+  /**
+   * Show the component; triggers the enter or exit states
+   */
+  in: PropTypes.bool,
+};
+
+function CollectionItem(props) {
+  const classes = useStyles();
+  const [summary, ...details] = props.children;
+
+  return (
+    <div>
+      <ListItem button
+        onClick={props.onClick}
+      >
+        <ListItemIcon>
+          {props.isExpanded ? <ExpandMoreIcon /> : <ChevronRight />}
+        </ListItemIcon>
         <ListItemText>
           {summary}
         </ListItemText>
-        {props.isExpanded ? <ExpandLessIcon /> : <ExpandMoreIcon />}
       </ListItem>
-      <Collapse
+      <TransitionComponent
         in={props.isExpanded}
         timeout="auto"
       >
-        {details}
-      </Collapse>
-    </>
+        <ListItem>
+          <ListItemText inset>
+            {details}
+          </ListItemText>
+        </ListItem>
+      </TransitionComponent>
+    </div>
   );
 }
 
@@ -86,6 +119,19 @@ function Collection(props) {
     </List>
   );
 }
+
+// === Functions ===
+
+const useStyles = makeStyles((theme) => ({
+  collectionItem: {
+    marginLeft: theme.spacing(2),
+    paddingLeft: theme.spacing(2),
+    borderLeft: `1px dashed ${fade(theme.palette.text.primary, 0.4)}`,
+  },
+  collectionItemSummary: {
+    paddingLeft: theme.spacing(2),
+  }
+}));
 
 // === Exports ===
 
