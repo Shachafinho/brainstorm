31199f2251b70d7f6999af0983af7a704dbe7294
Shachaf Ben Jakov <shachafinho@gmail.com>
2020-05-31 23:51:24 +0300

    Fix some deployment issues
    Update setup.py
    Reorder docker container builder
    Replace test example with something LIGHTER

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,10 +5,20 @@ cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
 
 function main {
+    # Setup Python
     python -m virtualenv .env --prompt "[brainstorm] "
     find .env -name site-packages -exec bash -c 'echo "../../../../" > {}/self.pth' \;
     .env/bin/pip install -U pip
     .env/bin/pip install -r requirements.txt
+
+    # Setup NodeJs
+    sudo apt install nodejs
+    sudo apt install npm
+    npm update
+    (cd brainstorm/gui/app/ && npm install)
+
+    # Setup GUI server files
+    (cd brainstorm/gui/app/ && npm run build)
 }
 
 
